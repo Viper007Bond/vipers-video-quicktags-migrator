73cb5623e9a7b1224bb0c8fa70b54ecdfedefbe0
Alex Mills <git@viper007bond.com>
2016-03-15 22:15:00 -0700

    Warn if VVQ is still activated and offer a link to deactivate it if so

--- vipers-video-quicktags-migrator.php
+++ vipers-video-quicktags-migrator.php
@@ -13,7 +13,38 @@ Author URI:   http://www.viper007bond.com/
 
 class VipersVideoQuicktagsMigrator {
 	function __construct() {
+		// To avoid weirdness, bail if the original plugin is still active
+		if ( class_exists( 'VipersVideoQuicktags' ) ) {
+			if ( current_user_can( 'activate_plugins' ) ) {
+				add_action( 'admin_notices', array( $this, 'display_vvq_active_warning' ) );
+			}
 
+			return;
+		}
+	}
+
+	public function display_vvq_active_warning() {
+		$vvq_file = 'vipers-video-quicktags/vipers-video-quicktags.php';
+
+		if ( in_array( $vvq_file, get_option( 'active_plugins', array() ) ) ) {
+			$deactivate_url = wp_nonce_url(
+				add_query_arg(
+					array(
+						'action' => 'deactivate',
+						'plugin' => rawurlencode( $vvq_file ),
+					),
+					admin_url( 'plugins.php' )
+				),
+				'deactivate-plugin_' . $vvq_file
+			);
+		} else {
+			$deactivate_url = add_query_arg( 's', rawurlencode( "Viper's Video Quicktags" ), admin_url( 'plugins.php' ) );
+		}
+
+		echo '<div class="notice notice-warning"><p>' . sprintf(
+				__( "<a href='%s'><strong>Please disable the Viper's Video Quicktags plugin.</strong></a> You have the migrator plugin installed and activated and it better handles all of the functionality of the old plugin.", 'vipers-video-quicktags-migrator' ),
+				esc_url( $deactivate_url )
+			) . '</p></div>';
 	}
 }
 
