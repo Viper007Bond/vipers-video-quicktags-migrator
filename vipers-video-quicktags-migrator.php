42eec9c0e1ac63152981588bece12ed4a6c036a4
Alex Mills <git@viper007bond.com>
2016-03-22 20:38:22 -0700

    The shortcodes accepted parameters in weird formats and just video IDs instead of video URLs, so each one will need some special handling. Introduce some helper functions for handling some of these cases.

--- vipers-video-quicktags-migrator.php
+++ vipers-video-quicktags-migrator.php
@@ -52,32 +52,103 @@ class VipersVideoQuicktagsMigrator {
 	public function add_shortcodes() {
 		// These ones need special handling, such as allowing a video ID instead of a full URL
 		add_shortcode( 'youtube', array( $this, 'shortcode_youtube' ) );
-		add_shortcode( 'dailymotion', array( $this, 'shortcode_dailymotion' ) );
-		add_shortcode( 'vimeo', array( $this, 'shortcode_vimeo' ) );
-		add_shortcode( 'veoh', array( $this, 'shortcode_veoh' ) );
-		add_shortcode( 'viddler', array( $this, 'shortcode_viddler' ) );
-		add_shortcode( 'metacafe', array( $this, 'shortcode_metacafe' ) );
-		add_shortcode( 'blip.tv', array( $this, 'shortcode_bliptv' ) );
-		add_shortcode( 'bliptv', array( $this, 'shortcode_bliptv' ) );
-		add_shortcode( 'flickrvideo', array( $this, 'shortcode_flickrvideo' ) );
-		add_shortcode( 'ifilm', array( $this, 'shortcode_ifilm' ) );
-		add_shortcode( 'spike', array( $this, 'shortcode_ifilm' ) );
-		add_shortcode( 'myspace', array( $this, 'shortcode_myspace' ) );
+		//add_shortcode( 'dailymotion', array( $this, 'shortcode_dailymotion' ) );
+		//add_shortcode( 'vimeo', array( $this, 'shortcode_vimeo' ) );
+		//add_shortcode( 'veoh', array( $this, 'shortcode_veoh' ) );
+		//add_shortcode( 'viddler', array( $this, 'shortcode_viddler' ) );
+		//add_shortcode( 'metacafe', array( $this, 'shortcode_metacafe' ) );
+		//add_shortcode( 'blip.tv', array( $this, 'shortcode_bliptv' ) );
+		//add_shortcode( 'bliptv', array( $this, 'shortcode_bliptv' ) );
+		//add_shortcode( 'flickrvideo', array( $this, 'shortcode_flickrvideo' ) );
+		//add_shortcode( 'ifilm', array( $this, 'shortcode_ifilm' ) );
+		//add_shortcode( 'spike', array( $this, 'shortcode_ifilm' ) );
+		//add_shortcode( 'myspace', array( $this, 'shortcode_myspace' ) );
 
 		// These services are dead
-		add_shortcode( 'googlevideo', array( $this, 'shortcode_dead_service' ) );
-		add_shortcode( 'gvideo', array( $this, 'shortcode_dead_service' ) );
-		add_shortcode( 'stage6', array( $this, 'shortcode_dead_service' ) );
+		//add_shortcode( 'googlevideo', array( $this, 'shortcode_dead_service' ) );
+		//add_shortcode( 'gvideo', array( $this, 'shortcode_dead_service' ) );
+		//add_shortcode( 'stage6', array( $this, 'shortcode_dead_service' ) );
 
 		// The rest of these can just be handled by WordPress core directly
-		add_shortcode( 'videofile', array( $GLOBALS['wp_embed'], 'shortcode' ) );
-		add_shortcode( 'video', array( $GLOBALS['wp_embed'], 'shortcode' ) );
-		add_shortcode( 'avi', array( $GLOBALS['wp_embed'], 'shortcode' ) );
-		add_shortcode( 'mpeg', array( $GLOBALS['wp_embed'], 'shortcode' ) );
-		add_shortcode( 'wmv', array( $GLOBALS['wp_embed'], 'shortcode' ) );
-		add_shortcode( 'flash', array( $GLOBALS['wp_embed'], 'shortcode' ) );
-		add_shortcode( 'flv', array( $GLOBALS['wp_embed'], 'shortcode' ) );
-		add_shortcode( 'quicktime', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		//add_shortcode( 'videofile', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		//add_shortcode( 'video', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		//add_shortcode( 'avi', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		//add_shortcode( 'mpeg', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		//add_shortcode( 'wmv', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		//add_shortcode( 'flash', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		//add_shortcode( 'flv', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		//add_shortcode( 'quicktime', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+	}
+
+	/**
+	 * A simple checker to see if a string looks like a URL or not.
+	 *
+	 * This function should NOT be used for security purposes, rather
+	 * it's just a quick and dirty way to tell video IDs from video URLs.
+	 *
+	 * @param string $string The string to check.
+	 *
+	 * @return bool Whether the string looks like a URL or not.
+	 */
+	public function is_url( $string ) {
+		return (bool) preg_match( '#^https?://#i', $string );
+	}
+
+	/**
+	 * Parse legacy formatted shortcodes into a more standardized format.
+	 *
+	 * Way back in the day, before shortcodes existed, WordPress.com created pseudo-shortcodes
+	 * that accepted no-name parameters (attributes) instead of between opening and closing tags.
+	 * This weird format is still in use to this day.
+	 *
+	 * Example: [youtube https://www.youtube.com/watch?v=EYs_FckMqow]
+	 *
+	 * With this format, the URL ends up being stored as $attr[0]. This helper function takes
+	 * that value and overwrites $content (where the URL should be), and then returns them both.
+	 *
+	 * @param string|array $attr    An empty string if there's no attributes in the shortcode, otherwise an array.
+	 * @param string       $content The existing shortcode content (between the shortcodes).
+	 *
+	 * @return array An array of the attributes and content variables.
+	 */
+	public function handle_no_name_attribute( $attr, $content ) {
+		if ( ! is_array( $attr ) || empty( $attr[0] ) ) {
+			return array( $attr, $content );
+		}
+
+		// Undo some of what wptexturize() did to the value
+		$find_and_replace = array(
+			'&#215;'  => 'x',
+			'&#8211;' => '--',
+			'&#8212;' => '---',
+			'&#8230;' => '...',
+			'&#8220;' => '"',
+			'&#8221;' => '"',
+			'&#8217;' => "'",
+			'&#038;'  => '&',
+		);
+		$attr[0] = str_replace( array_keys( $find_and_replace ), array_values( $find_and_replace ), $attr[0] );
+
+		// Equals sign between the shortcode tag and value with value inside of quotes
+		if ( preg_match( '#=("|\')(.*?)\1#', $attr[0], $match ) ) {
+			$content = $match[2];
+		}
+		// Equals sign between the shortcode tag and value with value unquoted
+		elseif ( '=' == substr( $attr[0], 0, 1 ) ) {
+			$content = substr( $attr[0], 1 );
+		}
+		// Normal with a space between the shortcode and the value
+		else {
+			$content = $attr[0];
+		}
+
+		unset( $attr[0] );
+
+		return array( $attr, $content );
+	}
+
+	public function shortcode_youtube( $attr, $content, $tag ) {
+		list( $attr, $content ) = $this->handle_no_name_attribute( $attr, $content );
 	}
 }
 
