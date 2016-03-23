12749419bb7d8310cf45d56ae20c6ae95440d705
Alex Mills <git@viper007bond.com>
2016-03-22 19:37:00 -0700

    The old shortcodes could optionally accept a video ID instead of a full URL, so we need a middleman shortcode handler

--- vipers-video-quicktags-migrator.php
+++ vipers-video-quicktags-migrator.php
@@ -50,35 +50,34 @@ class VipersVideoQuicktagsMigrator {
 	}
 
 	public function add_shortcodes() {
-		$shortcodes = array(
-			'youtube',
-			'googlevideo',
-			'gvideo',
-			'dailymotion',
-			'vimeo',
-			'veoh',
-			'viddler',
-			'metacafe',
-			'blip.tv',
-			'bliptv',
-			'flickrvideo',
-			'ifilm',
-			'spike',
-			'myspace',
-			'stage6',
-			'flv',
-			'quicktime',
-			'flash',
-			'videofile',
-			'video',
-			'avi',
-			'mpeg',
-			'wmv',
-		);
+		// These ones need special handling, such as allowing a video ID instead of a full URL
+		add_shortcode( 'youtube', array( $this, 'shortcode_youtube' ) );
+		add_shortcode( 'dailymotion', array( $this, 'shortcode_dailymotion' ) );
+		add_shortcode( 'vimeo', array( $this, 'shortcode_vimeo' ) );
+		add_shortcode( 'veoh', array( $this, 'shortcode_veoh' ) );
+		add_shortcode( 'viddler', array( $this, 'shortcode_viddler' ) );
+		add_shortcode( 'metacafe', array( $this, 'shortcode_metacafe' ) );
+		add_shortcode( 'blip.tv', array( $this, 'shortcode_bliptv' ) );
+		add_shortcode( 'bliptv', array( $this, 'shortcode_bliptv' ) );
+		add_shortcode( 'flickrvideo', array( $this, 'shortcode_flickrvideo' ) );
+		add_shortcode( 'ifilm', array( $this, 'shortcode_ifilm' ) );
+		add_shortcode( 'spike', array( $this, 'shortcode_ifilm' ) );
+		add_shortcode( 'myspace', array( $this, 'shortcode_myspace' ) );
 
-		foreach ( $shortcodes as $shortcode ) {
-			add_shortcode( $shortcode, array( $GLOBALS['wp_embed'], 'shortcode' ) );
-		}
+		// These services are dead
+		add_shortcode( 'googlevideo', array( $this, 'shortcode_dead_service' ) );
+		add_shortcode( 'gvideo', array( $this, 'shortcode_dead_service' ) );
+		add_shortcode( 'stage6', array( $this, 'shortcode_dead_service' ) );
+
+		// The rest of these can just be handled by WordPress core directly
+		add_shortcode( 'videofile', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		add_shortcode( 'video', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		add_shortcode( 'avi', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		add_shortcode( 'mpeg', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		add_shortcode( 'wmv', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		add_shortcode( 'flash', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		add_shortcode( 'flv', array( $GLOBALS['wp_embed'], 'shortcode' ) );
+		add_shortcode( 'quicktime', array( $GLOBALS['wp_embed'], 'shortcode' ) );
 	}
 }
 
